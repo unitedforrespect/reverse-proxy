a213e8b5b5cb3a749717b9ce080fd586e0ca79c7
Oren Goldberg <ogoldberg@gmail.com>
2023-05-01 15:43:33 -0400

    remove extraneous quotes

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: `"${process.env.SOURCE_PATH}/:path*"`,
+        source: `${process.env.SOURCE_PATH}/:path*`,
         destination: `${process.env.DESTINATION_URL}/:path*`,
       },
     ]
